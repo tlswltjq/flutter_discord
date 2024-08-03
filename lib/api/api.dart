5166add92af087bd2f271deed34a5f106c48d99b
rake_13@naver.com <wltjq1203@icloud.com>
2024-08-03 10:29:02 +0900

    로거 제거

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,18 +1,16 @@
 import 'package:flutter_discord/api/const.dart';
 import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
+
 abstract class ApiService{
   Future<http.Response> getServerList(String memberId);
   Future<http.Response> getChannelList(int serverId);
 }
 
 class ApiServiceImpl implements ApiService{
-  var logger = Logger();
   final String baseUrl = BASE_URL;
   
     @override
   Future<http.Response> getServerList(String memberId) async {
-    logger.d("serverList요청 함수 실행 :D");
     final url = Uri.parse('$baseUrl/server?memberId=$memberId');
     final response = await http.get(url);
     return response;
@@ -20,7 +18,6 @@ class ApiServiceImpl implements ApiService{
 
   @override
   Future<http.Response> getChannelList(int serverId) async {
-    logger.d("channelList요청 함수 실행 :D");
     final url = Uri.parse('$baseUrl/$serverId/channels');
     final response = await http.get(url);
     return response;
