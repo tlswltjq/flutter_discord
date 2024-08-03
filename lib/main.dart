04bffe0b138ade0263d1bbb44ee82109d29b80db
rake_13@naver.com <wltjq1203@icloud.com>
2024-08-03 10:29:13 +0900

    :D

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_discord/widget/HomeScreen.dart';
+import 'package:flutter_discord/page/HomeScreen.dart';
 
 void main() {
   runApp(DiscordApp());
 }
+
 class DiscordApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
