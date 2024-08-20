3d765cd7bb0105085f24f7d3a8469f2c62ad8530
rake_13@naver.com <wltjq1203@icloud.com>
2024-08-20 17:20:44 +0900

    getServerList의 로직과 반환 타입을 수정, TODO 추가

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,17 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter_discord/api/const.dart';
+import 'package:flutter_discord/domain/Server.dart';
 import 'package:http/http.dart' as http;
 
-class ApiService{
+class ApiService {
   final String baseUrl = BASE_URL;
-  
-    @override
-  Future<http.Response> getServerList(String memberId) async {
-    final url = Uri.parse('$baseUrl/server?memberId=$memberId');
-    final response = await http.get(url);
-    return response;
+
+  Future<List<Server>> getServerList(String memberId) async {
+    final response = await http.get(Uri.parse('$baseUrl/server?memberId=$memberId'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      List<Server> serverList =
+          jsonResponse.map((data) => Server.fromJson(data)).toList();
+      return serverList;
+    } else {
+      throw Exception('Failed to load server list');
+    }
   }
 
-  @override
+//todo : getChannelList메서드도 List<Channel>을 반환하도록 바디를 포함해 수정 필요
   Future<http.Response> getChannelList(int serverId) async {
     final url = Uri.parse('$baseUrl/$serverId/channels');
     final response = await http.get(url);
