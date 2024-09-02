b3c12c4d5051e5ef3ea351998a7a601f99e06183
rake_13@naver.com <wltjq1203@icloud.com>
2024-09-02 19:46:39 +0900

    grass.dart 작성

---
+++ lib/widgets/Grass.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Grass extends StatefulWidget{
+  @override
+  _GrassState createState() => _GrassState();
+}
+
+class _GrassState extends State<Grass> {
+  final List<int> log = [];//6개월간 기록을 저장할 변수
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
