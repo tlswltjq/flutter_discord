fc31b1e3aa99fedd67ef0102ea567cb831b135ef
rake_13@naver.com <wltjq1203@icloud.com>
2024-10-16 22:10:01 +1100

    Grass 작성중

--- lib/widgets/Grass.dart
+++ lib/widgets/Grass.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_diary/api/Api.dart';
+import 'package:flutter_diary/model/Activity.dart';
 
-class Grass extends StatefulWidget{
+class Grass extends StatefulWidget {
   @override
   _GrassState createState() => _GrassState();
 }
 
 class _GrassState extends State<Grass> {
-  final List<int> log = [];//6개월간 기록을 저장할 변수
+  final ApiService apiService = ApiService();
+  List<Activity> activityList = [];
 
+  @override
+  void initState() {
+    super.initState();
+    fetchActivities();
+  }
+
+  void fetchActivities() async {
+    try {
+      activityList = await apiService.getActivity(null);
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
   }
-  
 }
